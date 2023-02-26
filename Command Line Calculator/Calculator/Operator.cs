5dbb8b6b97f5942c87891f570ad8a8f3f2bf33d6
Zalth <102632056+Zalth@users.noreply.github.com>
2023-02-25 22:08:11 -0500

    Add files via upload
    Calculator programmed as a console app in C#.

---
+++ Command Line Calculator/Calculator/Operator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Operator
+    {
+        public static string Operate(string math)
+        {
+            bool resultOne = Regex.IsMatch(math.Trim(), @"(?i)^[a-z’'+\-\\/*x\s-]+$");
+            if (resultOne)
+            {
+                return math.ToUpper();
+            }  
+            else
+            {
+                return " ";
+            }    
+           
+
+        }
+    }
+}
---
+++ Command Line Calculator/Calculator/Program.cs
@@ -0,0 +1,79 @@
+﻿using Calculator;
+using System.IO.Pipes;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace calculator
+{
+    static class Collect
+    {
+        public static void Main(string[] args)
+        {
+            bool Continue = true;
+            do
+            {
+                try
+                {
+
+                    Console.WriteLine("Enter your first value.");
+                    var inputOne = Console.ReadLine();
+                    var numberOne = int.Parse(inputOne);
+                    Console.WriteLine("Enter your function (ie. Add, Subtract, Divide, Multiply)");
+                    string mathReturn = Console.ReadLine();
+
+                    Operator operate = new Operator();
+                    string Operation = Operator.Operate(mathReturn);
+
+                    Console.WriteLine("Enter your second value.");
+                    var inputTwo = Console.ReadLine();
+                    var numberTwo = int.Parse(inputTwo);
+
+                    Calc Calc = new Calc();
+
+                    if (Operation == "ADD" || Operation == "+")
+                    {
+                        Console.WriteLine("The answer is: " + Calc.Add(numberOne, numberTwo));
+                    }
+                    else if (Operation == "SUBTRACT" || Operation == "-")
+                    {
+                        Console.WriteLine("The answer is: " + Calc.Subtract(numberOne, numberTwo));
+                    }
+                    else if (Operation == "DIVIDE" || Operation == "÷" || Operation == "/")
+                    {
+                        Console.WriteLine("The answer is: " + Calc.Divide(numberOne, numberTwo));
+                    }
+                    else if (Operation == "MULTIPLY" || Operation == "x")
+                    {
+                        Console.WriteLine("The answer is: " + Calc.Multiply(numberOne, numberTwo));
+                    }
+                    else if (Operation == " ")
+                    {
+                        Console.WriteLine("You have entered an invalid operator");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please try again.");
+                    }
+
+
+                    Console.WriteLine("Do you want to go again?");
+                    string Answer = Console.ReadLine();
+                    String Answer2 = Answer.ToUpper();
+                    if (Answer2 == "YES")
+                    {
+                        Continue = true;
+                    }
+                    else if (Answer2 == "NO")
+                    {
+                        Continue = false;
+                    }
+                }catch(Exception ex)
+                {
+                    Console.WriteLine("An error has occured: "+ ex.Message);
+                }
+            } while (Continue == true);
+            
+        }
+    }    
+}
