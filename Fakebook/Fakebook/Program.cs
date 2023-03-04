808aaefcf506221b84d51401df1cf8e444c7bd0f
Zalth <102632056+Zalth@users.noreply.github.com>
2023-03-04 15:56:00 -0500

    Add files via upload
    Application I made for my Udemy course, collects fake "Facebook Likes" via command line, then displays the number of likes, as well as the names of those who liked.

---
+++ Fakebook/Fakebook/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace fakebook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var likes = new List<string>();
+            Console.WriteLine("Would you like to add a like?");
+            var response = Console.ReadLine();
+            while (response != "No")
+            {
+                Console.WriteLine("What is their name?");
+                var name = Console.ReadLine();
+                likes.Add(name);
+                Console.WriteLine("Would you like to add a like?");
+                response = Console.ReadLine();
+            }
+            Console.WriteLine("There are: " + likes.Count, " likes!");
+            Console.WriteLine("The people who liked the post are: ");
+            foreach (var n in likes)
+            {
+                Console.WriteLine(n.ToString());
+            }
+        }
+    }
+}
